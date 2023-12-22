b2bc212980b3d1a03d5d9ae8060b59e9db5df8a1
Olakunle64 <olakunleisiaq@gmail.com>
2023-12-22 20:29:29 +0000

    correction

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,5 +2,6 @@
 
 exec {'condition':
   command  => '/bin/pkill killmenow',
-  onlyif  => "/bin/ps aux | grep -q '[k]illmenow'",
+  onlyif   => "/bin/ps aux | grep -q '[k]illmenow'",
+  provider  => 'shell'
 }
