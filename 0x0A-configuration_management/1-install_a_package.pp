d72e33e5d6d55d9ae41b9fe6590d6889d5177df2
Olakunle64 <olakunleisiaq@gmail.com>
2023-12-22 20:26:30 +0000

    correct task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,24 @@
 # install the flask package with a specific version and provider
 
+# install python package of version 3.8.10
+package { 'python3':
+  ensure  => '3.8.10',
+}
+
+# ensure pip3 is install
+package { 'python3-pip':
+  ensure  => present,
+}
+# ensure pip is installed before installing flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip3'],
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# ensure flask is installed before installing werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
