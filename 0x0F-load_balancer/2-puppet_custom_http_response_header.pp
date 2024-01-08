d3cce86ab7508eacb3f8615a9489e6d73d929e97
Olakunle64 <olakunleisiaq@gmail.com>
2024-01-08 22:43:37 +0000

    write a manifest to add a header to a webserver

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,69 @@
+# creating a puppet to set up a nginx web server on a server
+# the nginx should listen to port 80
+# The redirection must be a "301 Moved Permanently"
+
+# update the server
+exec { 'apt-update':
+  command => 'apt-get -y update',
+  path    => '/usr/bin',
+}
+
+# install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# allow the webserver to listion on port 80
+exec { 'Nginx HTTP':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"',
+}
+
+# ensure this directory has the mode
+file { '/var/www':
+  ensure => 'directory',
+  mode   => '0755'
+}
+
+# ensure a /var/www/html is present
+file { '/var/www/html':
+  ensure => 'directory',
+}
+
+# create a file with a content
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!'
+}
+
+# configure the default server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "\
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+	add_header X-Served-By \${(hostname)}
+        location /redirect_me {
+                # First attempt to serve request as file, then
+                # add a redirection to a particular url
+                return 301 https://www.chess.com/;
+        }
+	location / {
+        	# First attempt to serve request as file, then
+        	# as directory, then fall back to displaying a 404.
+        	try_files \${uri} \${uri}/ =404;
+    	}	
+}",
+}
+
+# restart nginx
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
