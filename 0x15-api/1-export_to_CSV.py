79ec639ba63fe29d1d30dd23d1777310135bf8cc
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-01 00:25:46 +0000

    export data to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains Python script that, using an API, for a given
+    employee ID, export information about his/her TODO list progress
+    to a csv file.
+    """
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    csv_filename = f'{user_id}.csv'
+    todo = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+    user = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    USERNAME = user.json().get("name")
+    todo_list = todo.json()
+    full_csv_list = []
+    for each_todo in todo_list:
+        COMP_STATUS = each_todo.get("completed")
+        TITLE = each_todo.get("title")
+        data = f"\"{user_id}\",\"{USERNAME}\",\"{COMP__STATUS}\",\"{TITLE}\"\n"
+        full_csv_list.append(data)
+    with open(csv_filename, 'w', encoding="utf-8") as f:
+        for data in full_csv_list:
+            f.write(data)
