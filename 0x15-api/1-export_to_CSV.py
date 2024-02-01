ebf801e593416b6dfb574c6d058ac22955d0b3a7
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-01 01:50:48 +0000

    correct task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,36 +3,24 @@
     employee ID, export information about his/her TODO list progress
     to a csv file.
     """
-
 import requests
-import csv
 import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
+    csv_filename = f'{user_id}.csv'
     todo = requests.get(
             f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
     user = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{user_id}')
-    emp_name = user.json().get("name")
-    user_id = user.json().get("id")
-
-    todo_list = todo.json()
-
-    # Specify the CSV file name
-    csv_filename = f'{user_id}.csv'
-
-    # Open the CSV file in write mode
-    with open(csv_filename, 'w', newline='') as csvfile:
-        # Create a CSV writer object
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
-        # Write the data to the CSV file
-        for task in todo_list:
-            task_completed_status = str(task["completed"])
-            task_title = task["title"]
-
-            csv_writer.writerow(
-                    [user_id, emp_name, task_completed_status, task_title])
-
-    # print(f'Data has been written to {csv_filename}.')
+    USERNAME = user.json().get("username")
+    full_csv_list = []
+    for each_todo in todo.json():
+        COMP_STATUS = each_todo.get("completed")
+        TITLE = each_todo.get("title")
+        # user_id = user.get("userId")
+        data = f"\"{user_id}\",\"{USERNAME}\",\"{COMP_STATUS}\",\"{TITLE}\"\n"
+        full_csv_list.append(data)
+    with open(csv_filename, 'w', encoding="utf-8") as f:
+        for data in full_csv_list:
+            f.write(data)
