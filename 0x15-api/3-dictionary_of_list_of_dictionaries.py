8c0158acc54460d25b5177bc66ca3b5f40972c29
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-01 01:39:13 +0000

    add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module contains Python script that, using an API, for a given
+    employee ID, export information about his/her TODO list progress
+    in a json file.
+    """
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    filename = "todo_all_employees.json"
+    big_dict = {}
+    for user_id in range(1, 11):
+        todo = requests.get(
+                f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+        user = requests.get(
+                f'https://jsonplaceholder.typicode.com/users/{user_id}')
+        emp_name = user.json().get("name")
+        todo_dict = {}
+        todo_dict[str(user_id)] = []
+        todo_list = todo.json()
+        for each_todo in todo_list:
+            small_dict = {}
+            small_dict["task"] = str(each_todo["title"])
+            small_dict["completed"] = str(each_todo["completed"])
+            small_dict["username"] = str(emp_name)
+            todo_dict[str(user_id)].append(small_dict)
+        big_dict[str(user_id)] = todo_dict[str(user_id)]
+        with open(filename, "w") as f:
+            json.dump(big_dict, f)
