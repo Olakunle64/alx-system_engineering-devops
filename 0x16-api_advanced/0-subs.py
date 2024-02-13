d507c971a3e466ba1903ac2d169ca4112a13b8df
Olakunle64 <olakunleisiaq50@gmail.com>
2024-02-13 17:43:06 +0100

    query the reddit api and fetch the number of subscribers to a particular subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module contains a function that queries the Reddit API and
+    returns the number of subscribers (not active users, total
+    subscribers) for a given subreddit. If an invalid subreddit is
+    given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API
+
+        Args:
+            subreddit: a string
+
+        Return: return the number of subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "Chrome/97.0.4692.71"}
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code != 200:
+        return 0
+    return r.json()["data"]["subscribers"]
