4b313759ded501364cd9101f99b1e9d0a71fb306
Olakunle64 <olakunleisiaq50@gmail.com>
2024-02-13 18:25:37 +0100

    fetch the title of the top ten post of a particular subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ This module contains a function that queries the Reddit API and
-    returns the number of subscribers (not active users, total
-    subscribers) for a given subreddit. If an invalid subreddit is
-    given, the function should return 0.
+    prints the titles of the first 10 hot posts listed for a given
+    subreddit.
 """
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """queries the Reddit API
 
         Args:
@@ -15,9 +14,13 @@ def number_of_subscribers(subreddit):
 
         Return: return the number of subreddit
     """
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    url = "https://www.reddit.com/r/" + subreddit + "/new.json?limit=10"
     header = {"User-Agent": "Chrome/97.0.4692.71"}
     r = requests.get(url, headers=header, allow_redirects=False)
     if r.status_code != 200:
-        return 0
-    return r.json()["data"]["subscribers"]
+        print(None)
+    subreddit_info = r.json()["data"]
+    posts = subreddit_info["children"]
+    for post in posts:
+        post_title = post.get("data").get("title")
+        print(post_title)
