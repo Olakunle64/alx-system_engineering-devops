e29aa56883471a7bb890cad48a617543bd33c386
Olakunle64 <olakunleisiaq50@gmail.com>
2024-02-15 13:27:15 +0100

    edit task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,8 +16,11 @@ def number_of_subscribers(subreddit):
         Return: return the number of subreddit
     """
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    # header = {"User-Agent": "Chrome/97.0.4692.71"}
-    r = requests.get(url, allow_redirects=False)
+    headers = {"User-Agent": "Chrome/97.0.4692.71"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
         return 0
-    return r.json().get("data").get("subscribers")
+    suscribers = r.json().get("data").get("subscribers")
+    if suscribers is not None:
+        return r.json().get("data").get("subscribers")
+    return 0
