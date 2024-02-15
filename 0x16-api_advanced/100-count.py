af6b29d1e0ec4f44e8440b68a65f94c52830d5ce
Olakunle64 <olakunleisiaq50@gmail.com>
2024-02-15 16:40:08 +0100

    word count in title

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=""):
+def sub_count_words(subreddit, word_list, word_count, after=""):
     """queries the Reddit API
 
         Args:
@@ -15,25 +15,51 @@ def count_words(subreddit, word_list, after=""):
 
         Return: return the number of subreddit
     """
-    word_list = list(set(word_list))
-    values = [0] * len(word_list)
-    word_count = dict(zip(word_count, values))
-    url = "https://www.reddit.com/r/{}/new.json?after={}".format(
-            subreddit, after)
+    """
+    if not after:
+        unique_word_list = list(set(word_list))
+        values = [0] * len(word_list)
+        word_count = dict(zip(word_count, values))
+    """
+    if after:
+        url = "https://www.reddit.com/r/{}/new.json?after={}".format(
+                subreddit, after)
+    else:
+        url = "https://www.reddit.com/r/{}/new.json".format(subreddit)
     header = {"User-Agent": "Chrome/97.0.4692.71"}
     r = requests.get(url, headers=header, allow_redirects=False)
-    if r.status_code != 200:
-        print(None)
-    subreddit_info = r.json()["data"]
-    after = subreddit_info.get("after")
-    posts = subreddit_info["children"]
-    if posts:
-        post = posts.pop(0)
-        post_title = post.get("data").get("title")
-        # print(post_title)
-        hot_list.append(post_title)
-        return (recurse(subreddit, hot_list, after))
-    if not after:
-        return hot_list
+    if r.status_code == 200:
+        subreddit_info = r.json().get("data", {})
+        after = subreddit_info.get("after")
+        posts = subreddit_info.get("children", [])
+        for post in posts:
+            post_title = post.get("data").get("title")
+            # print(post_title)
+            for word in word_count.keys():
+                if word.lower() in post_title.lower():
+                    word_count[word] += 1
+        if not after:
+            return word_count
+        else:
+            # print(word_count)
+            return (sub_count_words(subreddit, word_list, word_count, after))
     else:
-        return (recurse(subreddit, hot_list, after))
+        return None
+
+
+def count_words(subreddit, word_list):
+    """return the word counts"""
+    unique_word_list = list(set(word_list))
+    values = [0] * len(word_list)
+    word_count = dict(zip(unique_word_list, values))
+    # print(word_count)
+    word_count_dict = sub_count_words(
+            subreddit, word_list, word_count, after="")
+    if word_count_dict:
+        sorted_word = sorted(
+                word_count_dict.items(), key=lambda item: (-item[1], item[0]))
+        for item in sorted_word:
+            if item[1]:
+                print("{}: {}".format(item[0].lower(), item[1]))
+    else:
+        pass
